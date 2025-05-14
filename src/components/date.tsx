e634a7261dfbbef43c1e53074b424e736a4d6796
Souvlaki42 <souvlaki420@gmail.com>
2025-05-14 15:16:50 +0300

    feat: added ui components

---
+++ src/components/date.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import { ComponentType, ReactNode, useEffect, useState } from "react";
+
+function withClient<T extends {}>(Comp: ComponentType<T>) {
+	return function ClientOnly(props: T & { children?: ReactNode }) {
+		const [isClient, setIsClient] = useState(false);
+		useEffect(() => {
+			setIsClient(true);
+		}, []);
+		return isClient ? <Comp {...props} /> : null;
+	};
+}
+
+const DateTime = ({ date }: { date?: string | null }) =>
+	!!date ? (
+		<time dateTime={date}>{new Date(date).toLocaleDateString()}</time>
+	) : (
+		"No date"
+	);
+
+export default withClient(DateTime);
