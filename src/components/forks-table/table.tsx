696c0b20fef2245e09f1f149ee0b0ac0674358de
Souvlaki42 <souvlaki420@gmail.com>
2025-05-14 22:06:57 +0300

    feat: changed components to finally make server side pagination work

---
+++ src/components/forks-table/table.tsx
@@ -0,0 +1,107 @@
+"use client";
+
+import {
+	ColumnFiltersState,
+	flexRender,
+	getCoreRowModel,
+	PaginationState,
+	SortingState,
+	useReactTable,
+} from "@tanstack/react-table";
+import { useState } from "react";
+import { FetchArgs, Fork, ForkResponse } from "~/lib/providers/common";
+import {
+	Table,
+	TableBody,
+	TableCell,
+	TableHead,
+	TableHeader,
+	TableRow,
+} from "../ui/table";
+import { columns } from "./columns";
+import { PaginationControls } from "./pagination";
+
+export function ForksTable({
+	forkResponse,
+	searchParams,
+}: {
+	forkResponse: ForkResponse<Fork> | null;
+	searchParams: Omit<Required<FetchArgs>, "repo">;
+}) {
+	const [pagination, setPagination] = useState<PaginationState>({
+		pageIndex: searchParams.page - 1,
+		pageSize: searchParams.perPage,
+	});
+	const [sorting, setSorting] = useState<SortingState>([]);
+	const [columnFilters, setColumnFilters] = useState<ColumnFiltersState>([]);
+
+	const table = useReactTable({
+		data: forkResponse?.forks ?? [],
+		columns,
+		getCoreRowModel: getCoreRowModel(),
+		manualPagination: true,
+		manualSorting: true,
+		manualFiltering: true,
+		rowCount: forkResponse?.pagination.total ?? 0,
+		state: { pagination, sorting, columnFilters },
+		onPaginationChange: setPagination,
+		onSortingChange: setSorting,
+		onColumnFiltersChange: setColumnFilters,
+	});
+
+	return (
+		<div>
+			<div className="rounded-md border">
+				<Table>
+					<TableHeader>
+						{table.getHeaderGroups().map((headerGroup) => (
+							<TableRow key={headerGroup.id}>
+								{headerGroup.headers.map((header) => {
+									return (
+										<TableHead key={header.id}>
+											{header.isPlaceholder
+												? null
+												: flexRender(
+														header.column.columnDef.header,
+														header.getContext()
+												  )}
+										</TableHead>
+									);
+								})}
+							</TableRow>
+						))}
+					</TableHeader>
+					<TableBody>
+						{table.getRowModel().rows?.length ? (
+							table.getRowModel().rows.map((row) => (
+								<TableRow
+									key={row.id}
+									data-state={row.getIsSelected() && "selected"}
+								>
+									{row.getVisibleCells().map((cell) => (
+										<TableCell key={cell.id}>
+											{flexRender(
+												cell.column.columnDef.cell,
+												cell.getContext()
+											)}
+										</TableCell>
+									))}
+								</TableRow>
+							))
+						) : (
+							<TableRow>
+								<TableCell
+									colSpan={columns.length}
+									className="h-24 text-center"
+								>
+									No data available for the table.
+								</TableCell>
+							</TableRow>
+						)}
+					</TableBody>
+				</Table>
+			</div>
+			<PaginationControls table={table} pagination={forkResponse?.pagination} />
+		</div>
+	);
+}
