0fbcd74006df60ea88b2ef80bf8f064029adc060
Souvlaki42 <souvlaki420@gmail.com>
2025-05-17 03:15:00 +0300

    feat: finished search repo feature

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import { Suspense } from "react";
 import { CardLayout } from "~/components/card-layout";
 import { ForksTable } from "~/components/forks-table/table";
+import { RepoSearchForm } from "~/components/repo-search";
 
 export default function Home() {
   return (
-    <CardLayout>
+    <CardLayout className="flex flex-col gap-4">
+      <RepoSearchForm />
       <Suspense fallback={<ForksTable loading />}>
         <ForksTable />
       </Suspense>
---
+++ src/components/repo-search.tsx
@@ -0,0 +1,59 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { usePathname, useRouter } from "next/navigation";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { z } from "zod";
+import { Button } from "~/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "~/components/ui/form";
+import { Input } from "~/components/ui/input";
+import { RepoSearchSchema } from "~/lib/providers/github/schema";
+
+export function RepoSearchForm() {
+  const pathname = usePathname();
+  const router = useRouter();
+
+  const onSubmit: SubmitHandler<z.infer<typeof RepoSearchSchema>> = (
+    values
+  ) => {
+    router.push(`/${values.repo}`);
+  };
+
+  const form = useForm<z.infer<typeof RepoSearchSchema>>({
+    resolver: zodResolver(RepoSearchSchema),
+    defaultValues: {
+      repo: pathname.substring(1),
+    },
+  });
+
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="flex items-end space-x-2"
+      >
+        <FormField
+          control={form.control}
+          name="repo"
+          render={({ field }) => (
+            <FormItem className="flex-1">
+              <FormLabel className="sr-only">Repo to search</FormLabel>
+              <FormControl>
+                <Input placeholder="techgaun/active-forks" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
+  );
+}
