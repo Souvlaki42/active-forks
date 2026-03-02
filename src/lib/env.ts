07040e3a62001c03dd0f95c815b83147b7f1412d
Souvlaki42 <ilias@moulas.dev>
2026-03-03 01:08:26 +0200

    chore: upgraded zod and removed redis

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -1,21 +1,12 @@
 import { createEnv } from "@t3-oss/env-nextjs";
 import { z } from "zod";
-import { urlPattern } from "./singletons/regex";
 
 export const env = createEnv({
   server: {
     GITHUB_API_TOKEN: z
-      .string({ description: "GitHub token" })
+      .string()
+      .describe("GitHub API token")
       .min(1, "GitHub token is required"),
-    UPSTASH_REDIS_REST_URL: z
-      .string({ description: "Redis URL" })
-      .refine((url) => urlPattern.test(url), "Invalid Redis URL"),
-    UPSTASH_REDIS_REST_TOKEN: z
-      .string({ description: "Redis token" })
-      .min(1, "Redis token is required"),
-    UPSTASH_REDIS_CACHE_TTL_SECONDS: z
-      .number({ description: "Seconds to cache data" })
-      .default(60 * 60 * 24),
   },
   client: {},
   experimental__runtimeEnv: {}, // Only required for client variables
