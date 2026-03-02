dd7f91ef9a942b7f0086771d8c5f55654c1d3425
Souvlaki42 <ilias@moulas.dev>
2026-03-03 01:10:30 +0200

    feat: added new github provider

---
+++ src/lib/github/forks.ts
@@ -0,0 +1,54 @@
+import { octokit } from "~/lib/singletons/octokit";
+import {
+  type ForkFetchArgs,
+  ForkFetchArgsSchema,
+  type ForkList,
+} from "./schema";
+
+export const getForks = async (args: ForkFetchArgs): Promise<ForkList> => {
+  console.log("getForks", args);
+  const { repo, page, per_page } = ForkFetchArgsSchema.parse(args);
+
+  if (!repo) return { total: 0, forks: [] };
+
+  const partRepo = repo.split("/");
+
+  const [owner, name] = partRepo;
+
+  const forkResponse = await octokit.rest.repos.listForks({
+    owner,
+    repo: name,
+    page,
+    per_page,
+    sort: "stargazers",
+  });
+
+  const searchResponse = await octokit.rest.search.repos({
+    q: repo,
+  });
+
+  const total = searchResponse.data.items[0].forks ?? 0;
+
+  const { data: forks } = forkResponse;
+
+  const response: ForkList = {
+    total,
+    forks: forks.map((fork) => {
+      return {
+        link: fork.html_url,
+        name: fork.name,
+        branch: fork.default_branch ?? "not available",
+        forks: fork.forks ?? 0,
+        owner: fork.owner.login,
+        avatar: fork.owner.avatar_url,
+        stars: fork.stargazers_count ?? 0,
+        size: fork.size ?? 0,
+        watchers: fork.watchers_count ?? 0,
+        openIssues: fork.open_issues_count ?? 0,
+        lastPush: fork.pushed_at,
+      };
+    }),
+  };
+
+  return response;
+};
---
+++ src/lib/github/schema.ts
@@ -0,0 +1,63 @@
+import { z } from "zod";
+import { repoPattern } from "../utils";
+
+export const ForkSchema = z.object({
+  link: z.string(),
+  owner: z.string(),
+  avatar: z.string(),
+  name: z.string(),
+  branch: z.string(),
+  stars: z.number(),
+  watchers: z.number(),
+  forks: z.number(),
+  openIssues: z.number(),
+  size: z.number().optional(),
+  lastPush: z.string().nullable().optional(),
+});
+export type Fork = z.infer<typeof ForkSchema>;
+
+export const ForkListSchema = z.object({
+  total: z.number(),
+  forks: z.array(ForkSchema),
+});
+export type ForkList = z.infer<typeof ForkListSchema>;
+
+export const RepoSchema = z
+  .string()
+  .refine((value) => repoPattern.test(value), {
+    message: "Invalid repo format! Please select a valid repo.",
+  })
+  .optional();
+
+export type Repo = z.infer<typeof RepoSchema>;
+
+export const ForkFetchArgsSchema = z.object({
+  repo: RepoSchema,
+  page: z.number().default(1),
+  per_page: z
+    .union([
+      z.literal(10),
+      z.literal(30),
+      z.literal(50),
+      z.literal(70),
+      z.literal(100),
+    ])
+    .default(30),
+});
+export type ForkFetchArgs = z.infer<typeof ForkFetchArgsSchema>;
+
+export const CompareArgsSchema = z.object({
+  repo: RepoSchema,
+  upstreamOwner: z.string(),
+  upstreamBranch: z.string().default("main"),
+  forkOwner: z.string(),
+  forkBranch: z.string().default("main"),
+});
+export type CompareArgs = z.infer<typeof CompareArgsSchema>;
+
+export const CompareResponseSchema = z.object({
+  ahead: z.number(),
+  behind: z.number(),
+  status: z.enum(["ahead", "behind", "equal"]),
+});
+export type CompareResponse = z.infer<typeof CompareResponseSchema>;
