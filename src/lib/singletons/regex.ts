eb9dc00e609d6e3a6eed81055e4b25b58d55ebc1
Souvlaki42 <ilias@moulas.dev>
2026-03-03 01:09:50 +0200

    refactor: removed a bunch of old files

--- src/lib/singletons/regex.ts
+++
@@ -1,4 +0,0 @@
-export const urlPattern =
-  /^(https?:\/\/)?((([a-z\d]([a-z\d-]*[a-z\d])*)\.)+[a-z]{2,}|((\d{1,3}\.){3}\d{1,3}))(\:\d+)?(\/[-a-z\d%_.~+]*)*(\?[;&a-z\d%_.~+=-]*)?(\#[-a-z\d_]*)?$/i;
-
-export const repoPattern = /^[a-zA-Z0-9_-]+\/[a-zA-Z0-9_-]+$/;
