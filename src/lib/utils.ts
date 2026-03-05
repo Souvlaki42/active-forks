e23596f1b86a11020bbd8b5a0899ca60011b137d
Souvlaki42 <ilias@moulas.dev>
2026-03-05 21:23:15 +0200

    refactor: moved custom fetch function to utils

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -2,7 +2,10 @@ import { rankItem } from "@tanstack/match-sorter-utils";
 import type { FilterFn } from "@tanstack/react-table";
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
+import { env } from "./env";
 import type { Fork } from "./github/schema";
+
+const REVALIDATE_MINUTES = 5;
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
@@ -13,6 +16,23 @@ export const fuzzyFilter: FilterFn<Fork> = (row, columnId, value, addMeta) => {
   addMeta({ itemRank });
   return itemRank.passed;
 };
+
+export const cachedFetch = (
+  url: string,
+  options: RequestInit,
+  tags?: string[],
+  revalidate: number = REVALIDATE_MINUTES,
+) =>
+  env.NODE_ENV === "development" && env.FORCE_DISABLE_CACHE
+    ? fetch(url, { ...options, cache: "no-store" })
+    : // WARNING: Data caching here only works when I use Vercel's infrastructure and local development.
+      fetch(url, {
+        ...options,
+        next: {
+          revalidate: revalidate * 60,
+          tags,
+        },
+      });
 
 type Success<T> = {
   data: T;
