e7ad1a1d3baddc7ee7731898f80c617e51f4fb1f
Souvlaki42 <ilias@moulas.dev>
2026-03-05 21:22:52 +0200

    feat: added env variable to locally disable fetch's cache

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -5,6 +5,7 @@ export const env = createEnv({
   server: {
     GITHUB_API_TOKEN: z.string().min(1, "GitHub token is required"),
     NODE_ENV: z.enum(["development", "production", "test"]),
+    FORCE_DISABLE_CACHE: z.boolean().default(false),
   },
   client: {},
   experimental__runtimeEnv: {}, // Only required for client variables
