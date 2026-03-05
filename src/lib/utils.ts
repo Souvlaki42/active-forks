4f5089ca5a92ac19b6474c16ae2f1f292425a72b
Souvlaki42 <ilias@moulas.dev>
2026-03-05 21:21:46 +0200

    refactor: removed a bunch of old utils

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,26 +1,12 @@
 import { rankItem } from "@tanstack/match-sorter-utils";
 import type { FilterFn } from "@tanstack/react-table";
 import { type ClassValue, clsx } from "clsx";
-import { toast } from "sonner";
 import { twMerge } from "tailwind-merge";
 import type { Fork } from "./github/schema";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
-
-export type AutoComplete<T extends string> = T | (string & {});
-
-export type Prettify<T> = {
-  [K in keyof T]: T[K];
-} & {};
-
-export type Unpromisify<T> = T extends Promise<infer U> ? U : T;
-
-export const copyToClipboard = (text: string) => {
-  navigator.clipboard.writeText(text);
-  toast.success("Copied to clipboard!");
-};
 
 export const fuzzyFilter: FilterFn<Fork> = (row, columnId, value, addMeta) => {
   const itemRank = rankItem(row.getValue(columnId), value);
