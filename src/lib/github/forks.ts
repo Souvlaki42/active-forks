4a691ec6a2265f0c0cdad0d23c32e29522d96c77
Souvlaki42 <ilias@moulas.dev>
2026-03-05 02:24:34 +0200

    refactor: removed octokit singleton to enable fetch caching

--- src/lib/github/forks.ts
+++ src/lib/github/forks.ts
@@ -1,9 +1,13 @@
-import { octokit } from "~/lib/singletons/octokit";
+import { Octokit } from "octokit";
+
+import { env } from "~/lib/env";
 import {
   type ForkFetchArgs,
   ForkFetchArgsSchema,
   type ForkList,
 } from "./schema";
+
+const REVALIDATE_MINUTES = 5;
 
 export const getForks = async (args: ForkFetchArgs): Promise<ForkList> => {
   console.info(`Fetching forks: ${JSON.stringify(args)}`);
@@ -12,9 +16,22 @@ export const getForks = async (args: ForkFetchArgs): Promise<ForkList> => {
 
   if (!repo) return { total: 0, forks: [] };
 
-  const partRepo = repo.split("/");
+  const [owner, name] = repo.split("/");
 
-  const [owner, name] = partRepo;
+  const octokit = new Octokit({
+    auth: env.GITHUB_API_TOKEN,
+    request: {
+      fetch: (url: string, options: RequestInit) =>
+        // WARNING: Data caching here only works when I use Vercel's infrastructure
+        fetch(url, {
+          ...options,
+          next: {
+            revalidate: REVALIDATE_MINUTES * 60,
+            tags: [`forks:${owner}/${name}`],
+          },
+        }),
+    },
+  });
 
   const forkRequest = octokit.rest.repos.listForks({
     owner,
@@ -28,11 +45,9 @@ export const getForks = async (args: ForkFetchArgs): Promise<ForkList> => {
 
   const APIResponse = await Promise.all([forkRequest, getRequest]);
 
-  const forks = APIResponse[0].data;
-
   const response: ForkList = {
     total: APIResponse[1].data.forks_count ?? 0,
-    forks: forks.map((fork) => {
+    forks: APIResponse[0].data.map((fork) => {
       return {
         link: fork.html_url,
         name: fork.name,
