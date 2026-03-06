3859e4dfa851020e31ede828703144a6d0bcedfb
Souvlaki42 <ilias@moulas.dev>
2026-03-06 16:27:41 +0200

    feat: added main element and bubble showing tailwind breakpoints

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import NextTopLoader from "nextjs-toploader";
 import { NuqsAdapter } from "nuqs/adapters/next/app";
 import { ThemeProvider } from "~/components/theme-provider";
 import "./globals.css";
+import { env } from "~/lib/env";
 
 const mainFont = DM_Sans({
   subsets: ["latin"],
@@ -30,7 +31,17 @@ export default function RootLayout({
             disableTransitionOnChange
           >
             <NextTopLoader color="var(--primary)" />
-            {children}
+            <main>{children}</main>
+            {env.NODE_ENV === "development" && (
+              <div className="fixed bottom-1 left-1 z-50 flex h-6 w-6 items-center justify-center rounded-full bg-gray-800 p-3 font-mono text-xs text-white">
+                <div className="block sm:hidden">xs</div>
+                <div className="hidden sm:block md:hidden">sm</div>
+                <div className="hidden md:block lg:hidden">md</div>
+                <div className="hidden lg:block xl:hidden">lg</div>
+                <div className="hidden xl:block 2xl:hidden">xl</div>
+                <div className="hidden 2xl:block">2xl</div>
+              </div>
+            )}
           </ThemeProvider>
         </NuqsAdapter>
       </body>
