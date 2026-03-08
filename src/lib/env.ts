a7ccf85f3976e110eb2c96c534f7ec4bbee3103e
Souvlaki42 <ilias@moulas.dev>
2026-03-08 18:49:06 +0200

    refactor: replaced @t3-oss/env-nextjs with pure zed

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -1,14 +1,7 @@
-import { createEnv } from "@t3-oss/env-nextjs";
 import { z } from "zod";
 
-export const env = createEnv({
-  server: {
-    GITHUB_API_TOKEN: z.string().min(1, "GitHub token is required"),
-    NODE_ENV: z.enum(["development", "production", "test"]),
-    FORCE_DISABLE_CACHE: z.boolean().default(false),
-  },
-  client: {},
-  experimental__runtimeEnv: {}, // Only required for client variables
-  emptyStringAsUndefined: true,
-  skipValidation: process.env.GITHUB_ACTIONS === "true",
+export const env = z.object({
+  GITHUB_API_TOKEN: z.string().min(1, "GitHub token is required"),
+  NODE_ENV: z.enum(["development", "production", "test"]),
+  FORCE_DISABLE_CACHE: z.boolean().default(false),
 });
