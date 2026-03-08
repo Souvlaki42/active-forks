f648bf2e311e11aa25a1c5fc6be50a6d32ff0800
Souvlaki42 <ilias@moulas.dev>
2026-03-09 00:48:28 +0200

    fix: forgot to setup env correctly

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -1,7 +1,11 @@
 import { z } from "zod";
 
-export const env = z.object({
+const envSchema = z.object({
   GITHUB_API_TOKEN: z.string().min(1, "GitHub token is required"),
   NODE_ENV: z.enum(["development", "production", "test"]),
   FORCE_DISABLE_CACHE: z.boolean().default(false),
 });
+
+export type Env = z.infer<typeof envSchema>;
+
+export const env = envSchema.parse(process.env);
