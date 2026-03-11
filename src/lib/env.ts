84ad91a60cab0a9d737fcee05565cbecf9495351
Souvlaki42 <ilias@moulas.dev>
2026-03-11 17:13:15 +0200

    fix: made force disable cache a boolean

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -5,7 +5,7 @@ export const env = createEnv({
   server: {
     GITHUB_API_TOKEN: z.string().min(1, "GitHub token is required"),
     NODE_ENV: z.enum(["development", "production", "test"]),
-    FORCE_DISABLE_CACHE: z.boolean().default(false),
+    FORCE_DISABLE_CACHE: z.stringbool().default(false),
   },
   client: {},
   experimental__runtimeEnv: {}, // Only required for client variables
