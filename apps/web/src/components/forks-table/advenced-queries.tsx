7a6276310b2cd462b90dbf8f044f5a1fb9706d05
Souvlaki42 <ilias@moulas.dev>
2026-03-15 00:05:23 +0200

    fix: trying to optimize react performance in tanstack table, query builder and nuqs combo

--- apps/web/src/components/forks-table/advenced-queries.tsx
+++ apps/web/src/components/forks-table/advenced-queries.tsx
@@ -1,28 +1,50 @@
 "use client";
 
-import { useId } from "react";
-import QueryBuilder from "react-querybuilder";
+import { createParser, useQueryState } from "nuqs";
+import { Suspense } from "react";
 import { camelCaseToTitleCase } from "~/lib/utils";
-import { QueryBuilderContext } from "../query-builder";
+import {
+  type Field,
+  formatQuery,
+  prepareRuleGroup,
+  QueryBuilder,
+  QueryBuilderContext,
+  type RuleGroupType,
+} from "../query-builder";
 import { columnList } from "./columns";
 
+const fields: Field[] = columnList.map((col) => ({
+  name: col,
+  label: camelCaseToTitleCase(col),
+}));
+
+const customAdvancedQueryParser = createParser<RuleGroupType>({
+  parse: (value) => {
+    let id = 0;
+    return prepareRuleGroup<RuleGroupType>(JSON.parse(value), {
+      idGenerator: () => {
+        return `aq-${id++}`;
+      },
+    });
+  },
+  serialize: (value) => {
+    return formatQuery(value, "json_without_ids");
+  },
+});
+
+const optionsWithDefault = customAdvancedQueryParser.withDefault({
+  id: "aq-0",
+  combinator: "and",
+  rules: [],
+});
+
 export function AdvancedQueries() {
-  const rootId = useId();
-
-  const fields = columnList.map((col) => ({
-    name: col,
-    label: camelCaseToTitleCase(col),
-  }));
-
-  const defaultQuery = {
-    id: rootId,
-    combinator: "and",
-    rules: [],
-  };
-
+  const [query, setQuery] = useQueryState("aq", optionsWithDefault);
   return (
     <QueryBuilderContext>
-      <QueryBuilder defaultQuery={defaultQuery} fields={fields} />
+      <Suspense fallback={<div>Loading...</div>}>
+        <QueryBuilder query={query} onQueryChange={setQuery} fields={fields} />
+      </Suspense>
     </QueryBuilderContext>
   );
 }
