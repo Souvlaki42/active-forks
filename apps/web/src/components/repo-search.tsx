fc8d5f984d4f96a97c44bc4e3c538edfc7b1af63
Souvlaki42 <ilias@moulas.dev>
2026-03-21 21:58:45 +0200

    fix: fixed unrelated user feedback issue and loading state issue in repo search form

--- apps/web/src/components/repo-search.tsx
+++ apps/web/src/components/repo-search.tsx
@@ -4,6 +4,7 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { Loader2 } from "lucide-react";
 import { usePathname, useSearchParams } from "next/navigation";
 import { useRouter } from "nextjs-toploader/app";
+import { useTransition } from "react";
 import { type SubmitHandler, useForm } from "react-hook-form";
 import { z } from "zod";
 import { Button } from "~/components/ui/button";
@@ -18,13 +19,16 @@ import {
 import { Input } from "~/components/ui/input";
 
 const RepoSearchSchema = z.object({
-  repo: z.string().includes("/"),
+  repo: z
+    .string()
+    .includes("/", { message: "Repo must not be empty and include a slash" }),
 });
 
 export function RepoSearchForm() {
   const pathname = usePathname();
   const router = useRouter();
   const searchParams = useSearchParams();
+  const [isPending, startTransition] = useTransition();
 
   const form = useForm<z.infer<typeof RepoSearchSchema>>({
     resolver: zodResolver(RepoSearchSchema),
@@ -36,11 +40,20 @@ export function RepoSearchForm() {
   const onSubmit: SubmitHandler<z.infer<typeof RepoSearchSchema>> = (
     values,
   ) => {
-    router.push(`/${values.repo.trim()}?${searchParams.toString()}`);
+    const formValue = values.repo.trim();
+    const defaultValue = form.formState.defaultValues?.repo?.trim() ?? "";
+    if (formValue === defaultValue) {
+      form.setError("repo", { message: "Already viewing this repository" });
+      return;
+    }
+    startTransition(() => {
+      router.push(`/${formValue}?${searchParams.toString()}`);
+    });
   };
 
   return (
     <Form {...form}>
+      <FormMessage />
       <form
         onSubmit={form.handleSubmit(onSubmit)}
         className="flex items-end space-x-2"
@@ -51,21 +64,18 @@ export function RepoSearchForm() {
           render={({ field }) => (
             <FormItem className="flex-1">
               <FormLabel className="sr-only">Repo to search</FormLabel>
+              <FormMessage />
               <FormControl>
                 <Input
                   placeholder='Find repository (e.g. "techgaun/active-forks")'
                   {...field}
                 />
               </FormControl>
-              <FormMessage />
             </FormItem>
           )}
         />
-        <Button
-          type="submit"
-          disabled={form.formState.isSubmitted || !form.formState.isDirty}
-        >
-          {form.formState.isSubmitted ? (
+        <Button type="submit" disabled={isPending}>
+          {isPending ? (
             <Loader2 className="text-secondary h-5 w-5 animate-spin" />
           ) : null}
           Search
